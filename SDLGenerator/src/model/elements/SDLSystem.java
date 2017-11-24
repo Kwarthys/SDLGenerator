347c883aadb39f04ec1be56c9d6df69b9b8fd680
Kwarthys <Kwarthys@gmail.com>
2017-11-24 09:56:52 +0100

    SDLSystem sketching and break taking

--- SDLGenerator/src/model/elements/SDLSystem.java
+++ SDLGenerator/src/model/elements/SDLSystem.java
@@ -1,11 +1,23 @@
 package model.elements;
 
+import java.util.ArrayList;
+
 import model.SDLElement;
 
 public class SDLSystem extends SDLElement {
+	
+	protected ArrayList<SDLProcess> processes = new ArrayList<>();
 
 	public SDLSystem(String name)
 	{
 		this.name = name;
+	}
+	
+	public void addProcess(SDLProcess p)
+	{
+		if(!processes.contains(p))
+		{
+			processes.add(p);
+		}
 	}
 }
