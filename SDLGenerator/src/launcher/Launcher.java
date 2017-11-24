b1d95ca2c50e88aa2fd3313ef0785e13ab8c159e
Kwarthys <Kwarthys@gmail.com>
2017-11-24 09:44:54 +0100

    First XMLParsing

--- SDLGenerator/src/launcher/Launcher.java
+++ SDLGenerator/src/launcher/Launcher.java
@@ -1,8 +1,13 @@
 package launcher;
 
+import java.io.File;
+
+import parser.ParseManager;
+
 public class Launcher {
 
 	public static void main(String[] args) {
+		ParseManager.parse(new File("TestXML.xml"));
 		System.out.println("perdu");
 	}
 
--- SDLGenerator/src/parser/ParseManager.java
+++ SDLGenerator/src/parser/ParseManager.java
@@ -1,5 +1,53 @@
 package parser;
 
-public class ParseManager {
+import java.io.File;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+public class ParseManager {
+	
+	public static void parse(File toParse)
+	{
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		
+		DocumentBuilder documentBuilder;
+		try{
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			Document document = documentBuilder.parse(toParse);
+			
+			Node system = document.getElementsByTagName("System").item(0);
+			
+			System.out.println(getItemName(system));
+			
+			Node process = document.getElementsByTagName("Process").item(0);
+			
+			System.out.println(getItemName(process));
+			
+			//String usr = document.getElementsByTagName("user").item(0).getTextContent();
+			//String pwd = document.getElementsByTagName("password").item(0).getTextContent();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Epic fail");
+		}		
+	}
+	
+	
+	protected static String getItemName(Node node)
+	{
+		for(int i = 0; i < node.getChildNodes().getLength(); i++)
+		{
+			Node currentNode = node.getChildNodes().item(i);
+			if(currentNode.getNodeName().compareTo("name") == 0)
+			{
+				return currentNode.getTextContent();
+			}
+		}
+		
+		return null;
+	}
 }
